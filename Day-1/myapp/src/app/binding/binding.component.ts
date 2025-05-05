5715f3d1e2e712531cce0356b46b75f4223e4101
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-05-05 17:35:18 +0530

    typescript,Angular Application,Binding

--- Day-1/myapp/src/app/binding/binding.component.ts
+++ Day-1/myapp/src/app/binding/binding.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class BindingComponent {
 
   name:string="SARB Application";
+
+  products:any=[
+    {name:"pencile",price:"10",availability:"05-05-2025",rating:4.8},
+    {name:"eraser",price:"5",availability:"02-05-2025",rating:4.7},
+    {name:"scale",price:"8",availability:"10-05-2025",rating:5.0},
+    {name:"books",price:"20",availability:"23-05-2025",rating:4.6},
+    {name:"diary",price:"50",availability:"09-05-2025",rating:5.0},
+  
+  ]
+
 }
