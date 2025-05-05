f1f19378640347ac42e8dc14d2e63396bba65591
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-05-05 13:39:02 +0530

    typescript

---
+++ Day-1/first.js
@@ -0,0 +1,10 @@
+var a;
+a = 56;
+var b = 12;
+var fname = "Nikunj Soni";
+console.log(a + b);
+function test(num1, num2) {
+    return num1 + num2;
+}
+console.log(fname);
+console.log("Addition:", test(10, 34));
---
+++ Day-1/first.ts
@@ -0,0 +1,15 @@
+let a:number;
+a=56;
+
+let b:number=12;
+
+let fname:string="Nikunj Soni";
+
+console.log(a+b);
+
+function test(num1:number,num2:number){
+    return num1+num2;
+}
+
+console.log(fname);
+console.log("Addition:",test(10,34));
