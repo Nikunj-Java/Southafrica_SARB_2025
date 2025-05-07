698e3fd3a30e807cbbb29c3d8eb41ebf7095e731
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-05-07 19:49:55 +0530

    Todo-List,Pipes,Custom Pipes,Custom dates,Reactive Forms,Todo List App With CRUD Operation,Routing

--- Day-3/Angular-App/src/app/app-routing.module.ts
+++ Day-3/Angular-App/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PipesComponent } from './pipes/pipes.component';
+import { RformComponent } from './rform/rform.component';
+import { TformComponent } from './tform/tform.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:PipesComponent},
+  {path:"rform",component:RformComponent},
+  {path:"tform",component:TformComponent},
+  {path:"todo",component:TodoComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
